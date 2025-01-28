756b9090c76e8213b41c9b09d43e467139b1a962
Yulisa10 <yortizg@unal.edu.co>
2025-01-28 18:40:33 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -1,11 +1,64 @@
 import streamlit as st
-
+from PIL import Image
+import numpy as np
 def main():
   st.title("Clasificación de la base de datos MNIST")
   st.markdown("Sube una imagen para clasificar")
 
   uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
+
+  if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption = "imagen subida")
   
 
 if __name__ == "__main__":
   main()
+Nubia Esteban Duarte
+18:31
+import streamlit as st
+from PIL import Image
+from tensorflow.keras.preprocessing.image import img_to_array
+
+def preprocess_image(image):
+  image = image.convert('L') # convertir a escala de grises
+  image = image.resize((28,28))
+  image_array = img_to_array(image) / 255.0
+  image_array = np.expand_dims(image_array, axis=0)
+  return image_array
+
+def main():
+  st.title("Clasificación de la base de datos MNIST")
+  st.markdown("Sube una imagen para clasificar")
+
+  uploaded_file = st.file_uploader("S
+import streamlit as st
+from PIL import Image
+from tensorflow.keras.preprocessing.image import img_to_array
+
+def preprocess_image(image):
+  image = image.convert('L') # convertir a escala de grises
+  image = image.resize((28,28))
+  image_array = img_to_array(image) / 255.0
+  image_array = np.expand_dims(image_array, axis=0)
+  return image_array
+
+def main():
+  st.title("Clasificación de la base de datos MNIST")
+  st.markdown("Sube una imagen para clasificar")
+
+  uploaded_file = st.file_uploader("S
+elecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
+
+  if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption = "imagen subida")
+
+    preprocessed_image = preprocess_image(image)
+    
+    st.image(preprocessed_image, caption = "imagen subida")
+  
+
+if __name__ == "__main__":
+  main()
+
